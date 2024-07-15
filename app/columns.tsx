04dc5b9d518af1f2a3fbc0a91fc39db091c44298
Pierre Barbé <pierrebarbe0@gmail.com>
2024-07-15 17:34:44 +0200

    update: crypto table

--- app/columns.tsx
+++ app/columns.tsx
@@ -1,43 +1,77 @@
-"use client";
+'use client';
 
-import { ColumnDef } from "@tanstack/react-table";
-import { CoinTable } from "@/types/Coin";
-import { ArrowUpDown, MoreHorizontal } from "lucide-react";
-import { Button } from "@/components/ui/button";
+import { ColumnDef } from '@tanstack/react-table';
+import { CoinTable } from '@/types/Coin';
+import { ArrowUpDown, MoreHorizontal } from 'lucide-react';
+import { Button } from '@/components/ui/button';
 
 export const columns: ColumnDef<CoinTable>[] = [
-  {
-    accessorKey: "name",
-    header: "Name",
-  },
-  {
-    accessorKey: "symbol",
-    header: "Symbol",
-  },
-  {
-    accessorKey: "current_price",
-    header: ({ column }) => {
-      return (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Price
-          <ArrowUpDown className="ml-2 h-4 w-4" />
-        </Button>
-      );
-    },
-  },
-  {
-    accessorKey: "market_cap",
-    header: "Market Cap",
-  },
-  {
-    accessorKey: "market_cap_change_percentage_24h",
-    header: "24h Change",
-  },
-  {
-    accessorKey: "circulating_supply",
-    header: "Circulating Supply",
-  },
+	{
+		accessorKey: 'name',
+		header: 'Name'
+	},
+	{
+		accessorKey: 'symbol',
+		header: 'Symbol',
+		cell: ({ getValue }) => {
+			const value = getValue() as string;
+			return value.toUpperCase();
+		}
+	},
+	{
+		accessorKey: 'current_price',
+		header: ({ column }) => {
+			return (
+				<Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}>
+					Price
+				</Button>
+			);
+		},
+		cell: ({ getValue }) => {
+			const value = getValue() as number | null;
+			return value ? value.toFixed(2) + '$' : '-';
+		}
+	},
+	{
+		accessorKey: 'market_cap',
+		header: ({ column }) => {
+			return (
+				<Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}>
+					Market Cap
+				</Button>
+			);
+		},
+		cell: ({ getValue }) => {
+			const value = getValue() as number | null;
+			return value ? value.toFixed(0) + ' $' : '-';
+		}
+	},
+	{
+		accessorKey: 'market_cap_change_percentage_24h',
+		header: ({ column }) => {
+			return (
+				<Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}>
+					24h Change
+				</Button>
+			);
+		},
+		cell: ({ getValue }) => {
+			const value = getValue() as number | null;
+			return value ? value.toFixed(2) + '%' : '-';
+		}
+	},
+	{
+		accessorKey: 'circulating_supply',
+		header: ({ column }) => {
+			return (
+				<Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}>
+					Circulating Supply
+				</Button>
+			);
+		},
+		cell: ({ getValue }) => {
+			const value = getValue() as number | null;
+			return value ? value.toFixed(0) : '-';
+		}
+	}
 ];
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,17 @@
-import { Coin } from "@/types/Coin";
-import { columns } from "./columns";
-import { DataTable } from "./data-table";
+import { Coin } from '@/types/Coin';
+import { columns } from './columns';
+import { DataTable } from './data-table';
 
 export default async function Home() {
-  const crypto = await fetch(
-    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=1h"
-  );
-  const cryptoData: Coin[] = await crypto.json();
+	const crypto = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=1h');
+	const cryptoData: Coin[] = await crypto.json();
 
-  return (
-    <main className="flex min-h-screen flex-col items-center ">
-      <h1>FinanceFlow</h1>
-      <div className="mt-10 w-3/4">
-      <DataTable columns={columns} data={cryptoData}  />
-      </div>
-    </main>
-  );
+	return (
+		<main className="flex min-h-screen flex-col items-center ">
+			<h1>FinanceFlow</h1>
+			<div className="mt-10 w-3/4">
+				<DataTable columns={columns} data={cryptoData} />
+			</div>
+		</main>
+	);
 }
