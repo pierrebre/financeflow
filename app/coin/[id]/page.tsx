f6afd6834925f628645273f4656712969ff2cd2a
Pierre Barbé <pierrebarbe0@gmail.com>
2024-07-17 12:46:25 +0200

    feat: add chart component and type

--- app/coin/[id]/page.tsx
+++ app/coin/[id]/page.tsx
@@ -1,4 +1,5 @@
-import type { Coin } from '@/types/Coin';
+import type { Coin, DataPrice } from '@/types/Coin';
+import { Chart } from '@/components/chart';
 
 type Props = {
 	readonly params: {
@@ -27,13 +28,26 @@ export default async function Coin({ params }: Props) {
 
 	const coin = await getCoin();
 
+	const priceData: DataPrice[] = [
+		{ month: 'January', price: 18500 },
+		{ month: 'February', price: 32500 }, 
+		{ month: 'March', price: 40000 },
+		{ month: 'April', price: 46750 },
+		{ month: 'May', price: 50000 },
+		{ month: 'June', price: 54500 },
+	];
+
 	return (
-		<main className="">
-			<h1 className="scroll-m-20 text-3xl bold tracking-tight lg:text-4xl first-letter:capitalize mb-2">{params.id}</h1>
-			<p className="lg:text-2xl text-xl font-extrabold">
-				{'$ ' + coin.current_price.toFixed(2)} <span className="text-green-800 font-medium lg:text-lg"> {coin.price_change_percentage_24h.toFixed(2)}% </span>
-			</p>
-			<section></section>
+		<main className="flex lg:flex-row flex-col">
+			<section className=' border-gray-200 w-1/4'>
+				<h1 className="scroll-m-20 text-3xl bold tracking-tight lg:text-4xl first-letter:capitalize mb-2">{params.id}</h1>
+				<p className="lg:text-2xl text-xl font-extrabold">
+					{'$ ' + coin.current_price.toFixed(2)} <span className="text-green-800 font-medium lg:text-lg"> {coin.price_change_percentage_24h.toFixed(2)}% </span>
+				</p>
+			</section>
+			<section className='lg:w-3/4 w-full lg:mt-0 mt-10'>
+				<Chart priceData={priceData} />
+			</section>
 		</main>
 	);
 }
