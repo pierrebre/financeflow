8ccfb409cc9a0727e388a47f1e374df356da2bf6
Pierre Barbé <pierrebarbe0@gmail.com>
2024-07-22 16:41:31 +0200

    feat: implemantion of next auth

---
+++ components/sign-in.tsx
@@ -0,0 +1,17 @@
+import { signIn } from '../app/auth';
+import { CircleUserIcon } from 'lucide-react';
+
+export function SignIn() {
+	return (
+		<form
+			action={async () => {
+				'use server';
+				await signIn('google');
+			}}
+		>
+			<button type="submit">
+				<CircleUserIcon className="h-10 w-10" strokeWidth={1.5} />
+			</button>
+		</form>
+	);
+}
