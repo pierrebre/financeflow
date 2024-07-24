24aec55601a61d7e12c16cb1f5e1195c81238466
Pierre Barbé <pierrebarbe0@gmail.com>
2024-07-24 12:01:35 +0200

    fix: session management

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,2 @@
-import { handlers } from '../../../auth'; // Correct import path for auth.ts
+import { handlers } from '../../../(auth)/auth'; // Correct import path for auth.ts
 export const { GET, POST } = handlers;
