ded3d93fd0f7ea6492fa6266681c16f947e55760
Pierre Barbé <pierrebarbe0@gmail.com>
2024-08-04 16:57:18 +0200

    feat: display crypto details on favorites page

--- lib/api.ts
+++ lib/api.ts
@@ -62,3 +62,37 @@ export const fetchPriceHistory = async (coinId: string, interval: string): Promi
 		price: parseFloat(price.toFixed(2))
 	}));
 };
+
+export const fetchCoinsWatchlist = async (coinId: string[]): Promise<Coin[]> => {
+	const response = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${coinId}&order=market_cap_desc&per_page=20&page=1&sparkline=false&price_change_percentage=1h`);
+	const data = await response.json();
+
+	return data.map((coin: Coin) => ({
+		id: coin.id,
+		name: coin.name,
+		symbol: coin.symbol,
+		image: coin.image,
+		current_price: coin.current_price,
+		market_cap: coin.market_cap,
+		market_cap_rank: coin.market_cap_rank,
+		fully_diluted_valuation: coin.fully_diluted_valuation,
+		total_volume: coin.total_volume,
+		high_24h: coin.high_24h,
+		low_24h: coin.low_24h,
+		price_change_24h: coin.price_change_24h,
+		price_change_percentage_24h: coin.price_change_percentage_24h,
+		market_cap_change_24h: coin.market_cap_change_24h,
+		market_cap_change_percentage_24h: coin.market_cap_change_percentage_24h,
+		circulating_supply: coin.circulating_supply,
+		total_supply: coin.total_supply,
+		max_supply: coin.max_supply,
+		ath: coin.ath,
+		ath_change_percentage: coin.ath_change_percentage,
+		ath_date: coin.ath_date,
+		atl: coin.atl,
+		atl_change_percentage: coin.atl_change_percentage,
+		atl_date: coin.atl_date,
+		roi: coin.roi,
+		last_updated: coin.last_updated
+	}));
+};
