cbabdb8ce9752e7e6225df5b1f36242f6c386575
Pierre Barbé <pierrebarbe0@gmail.com>
2024-08-25 16:05:44 +0200

    feat: add user for id

--- app/(auth)/auth.ts
+++ app/(auth)/auth.ts
@@ -2,15 +2,20 @@ import NextAuth from 'next-auth';
 import Google from 'next-auth/providers/google';
 import GitHub from 'next-auth/providers/github';
 
+import { hashString } from '@/lib/utils';
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
 	providers: [Google, GitHub],
 	callbacks: {
-		session: ({ session, token }) => ({
-		  ...session,
-		  user: {
-			...session.user,
-			id: token.sub,
-		  },
-		}),
-	  },
+		session: async ({ session, token }) => {
+			const userId = await hashString(session.user?.email ?? '');
+			return {
+				...session,
+				user: {
+					...session.user,
+					id: userId
+				}
+			};
+		}
+	}
 });
