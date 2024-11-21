06c05a8f5da60b2eb49c3217a9dee77d71bdebec
Pierre Barbé <pierrebarbe0@gmail.com>
2024-11-21 04:01:05 +0100

    fix: ts build errors

--- components/auth/new-verification-form.tsx
+++ components/auth/new-verification-form.tsx
@@ -13,7 +13,7 @@ export const NewVerificationForm = () => {
 	const [success, setSuccess] = useState<string | undefined>();
 
 	const searchParams = useSearchParams();
-	const token = searchParams.get('token');
+	const token = searchParams?.get('token');
 
 	const onSubmit = useCallback(() => {
 		if (error || success) return;
