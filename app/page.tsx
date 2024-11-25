a237407ba3335f0823f239faf583134737133552
Pierre Barbé <pierrebarbe0@gmail.com>
2024-11-25 04:02:55 +0100

    feat(page): update home page structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,44 @@
 'use client';
-import { fetchCryptos } from '@/lib/utils';
+import { fetchCryptos } from '@/data/coin';
 import { columns } from '../components/dataTable/columns';
 import { DataTable } from '../components/dataTable/data-table';
 import { useInfiniteQuery } from '@tanstack/react-query';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { Coin } from '@/schemas';
 
 export default function Home() {
 	const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isError } = useInfiniteQuery<Coin[], Error>({
 		queryKey: ['cryptos'],
 		queryFn: ({ pageParam = 1 }) => fetchCryptos({ pageParam: pageParam as number }),
-		getNextPageParam: (lastPage, pages) => (pages.length < 4 && lastPage.length === 20 ? pages.length + 1 : undefined),
+		getNextPageParam: (lastPage, pages) => (pages.length < 5 && lastPage.length === 20 ? pages.length + 1 : undefined),
 		initialPageParam: 1
 	});
 
 	const cryptoData = data?.pages.flatMap((page) => page) || [];
 
-	const [isNearBottom, setIsNearBottom] = useState(false);
-
 	useEffect(() => {
-		const handleScroll = () => {
-			const bottom = Math.ceil(window.innerHeight + window.scrollY) >= document.documentElement.scrollHeight - 200;
-			setIsNearBottom(bottom);
-		};
+		const observer = new IntersectionObserver(
+			(entries) => {
+				if (entries[0].isIntersecting && hasNextPage && !isFetchingNextPage && !isError) {
+					fetchNextPage();
+				}
+			},
+			{ threshold: 0.1 }
+		);
 
-		window.addEventListener('scroll', handleScroll);
-		return () => window.removeEventListener('scroll', handleScroll);
-	}, []);
-
-	useEffect(() => {
-		if (isNearBottom && hasNextPage && !isFetchingNextPage && !isError) {
-			fetchNextPage();
+		const sentinel = document.getElementById('scroll-sentinel');
+		if (sentinel) {
+			observer.observe(sentinel);
 		}
-	}, [isNearBottom, hasNextPage, isFetchingNextPage, fetchNextPage, isError]);
+
+		return () => observer.disconnect();
+	}, [hasNextPage, isFetchingNextPage, fetchNextPage, isError]);
 
 	return (
 		<main className="flex flex-col items-center">
 			<div className="w-full">
 				<DataTable columns={columns} data={cryptoData} isLoading={isFetchingNextPage} />
+				<div id="scroll-sentinel" className="h-[10px]" />
 			</div>
 		</main>
 	);
