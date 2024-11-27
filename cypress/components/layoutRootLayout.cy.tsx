a6975232cebd241fafe1af2832489d7b375ac360
Pierre Barbé <pierrebarbe0@gmail.com>
2024-11-27 23:54:58 +0100

    fix: obsolete test

--- cypress/components/layoutRootLayout.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import RootLayout from '../../app/layout';
-
-describe('<RootLayout />', () => {
-	it('renders', () => {
-		// see: https://on.cypress.io/mounting-react
-		cy.mount(<RootLayout children={<div>Hello</div>} session={null} />);
-	});
-});
