428bade3bc9a70c0832d28251a9e3d4277f6db4f
Pierre Barbé <pierrebarbe0@gmail.com>
2025-01-06 22:01:35 -0500

    feat: add portfolio selection component and improve portfolio management interface

--- components/dashboard/portfolio-list.tsx
+++ components/dashboard/portfolio-list.tsx
@@ -2,7 +2,10 @@
 
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { PortfolioDialog } from './portfolio-dialog';
-import { useOptimistic } from 'react';
+import { useOptimistic, useState } from 'react';
+import { Plus } from 'lucide-react';
+import { Button } from '../ui/button';
+import PortfolioSelect from './portfolio-select';
 
 interface Portfolio {
 	id: string;
@@ -20,6 +23,18 @@ interface PortfolioListProps {
 
 export default function PortfolioList({ initialPortfolios, userId }: Readonly<PortfolioListProps>) {
 	const [optimisticPortfolios, addOptimisticPortfolio] = useOptimistic<Portfolio[], Portfolio>(initialPortfolios, (state, newPortfolio) => [...state, newPortfolio]);
+	const [selectedPortfolio, setSelectedPortfolio] = useState<Portfolio | null>(null);
+	const [error, setError] = useState<string | null>(null);
+
+	function handlePortfolioSelection(id: string) {
+		const portfolio = optimisticPortfolios.find((p) => p.id === id);
+		if (!portfolio) {
+			setError('Portfolio not found');
+			return;
+		}
+		setError(null);
+		setSelectedPortfolio(portfolio);
+	}
 
 	return (
 		<Card>
@@ -28,20 +43,17 @@ export default function PortfolioList({ initialPortfolios, userId }: Readonly<Po
 				<CardDescription>Manage your portfolios</CardDescription>
 			</CardHeader>
 			<CardContent>
-				<div className="flex space-x-4">
-					{optimisticPortfolios.length > 0 ? (
-						optimisticPortfolios.map((portfolio) => (
-							<span key={portfolio.id} className="">
-								{portfolio.name}
-							</span>
-						))
-					) : (
-						<div className="text-center py-4 text-muted-foreground">No portfolios yet</div>
-					)}
-				</div>
-				<div className="mt-4">
+				<div className="mb-4">
 					<PortfolioDialog userId={userId} onOptimisticAdd={addOptimisticPortfolio} />
 				</div>
+				<div className="flex space-x-4">
+					<PortfolioSelect optimisticPortfolios={optimisticPortfolios} selectedPortfolio={selectedPortfolio} onSelect={handlePortfolioSelection} />
+					<Button variant="outline" className="ml-auto" disabled={!selectedPortfolio}>
+						<Plus />
+					</Button>
+				</div>
+				{error && <p className="text-red-500">{error}</p>}
+				{selectedPortfolio ? <p>Selected Portfolio: {selectedPortfolio.name}</p> : <p>No portfolio selected</p>}
 			</CardContent>
 		</Card>
 	);
