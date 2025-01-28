ab748bce0afe99f85d78b35128216b8de06ba81b
Pierre Barbé <pierrebarbe0@gmail.com>
2025-01-27 21:41:34 -0500

    `feat: enhance blog card animation with hover transition effects`

--- components/blog/blog-card.tsx
+++ components/blog/blog-card.tsx
@@ -8,13 +8,12 @@ interface ContentCardPropsType {
 
 export function BlogCard({ img, title, link }: ContentCardPropsType) {
 	return (
-		<Card className="relative grid min-h-[30rem] items-end overflow-hidden rounded-xl" color="transparent">
+		<Card className="relative grid min-h-[30rem] items-end overflow-hidden rounded-xl group" color="transparent">
 			<a href={link ?? '/'} target="_blank" rel="noopener noreferrer">
-				<img src={img} alt="bg" className="absolute inset-0 h-full w-full object-cover object-center" />
-				<div className="absolute inset-0 bg-black/70" />
-				<CardContent className="relative flex flex-col justify-end">
+				<img src={img} alt="bg" className="absolute inset-0 h-full w-full object-cover object-center transition-transform duration-500 group-hover:scale-105" />
+				<div className="absolute inset-0 bg-black/70 transition-opacity duration-500 group-hover:bg-black/60" />
+				<CardContent className="relative flex flex-col justify-end transform transition-transform duration-500 group-hover:translate-y-[-8px]">
 					<h4 className="text-white text-3xl font-bold">{title}</h4>
-					<p className="text-white text-sm">Published on 2023-01-01</p>
 				</CardContent>
 			</a>
 		</Card>
